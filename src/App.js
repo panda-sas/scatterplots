7b02c454ad0e31462385a329a66a7b24f7226b21
Saswati Panda <panda.sas20@gmail.com>
2022-08-22 15:10:59 -0500

    Scatterplot echart validation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "primereact/resources/themes/lara-light-indigo/theme.css"; //theme
+import "primereact/resources/primereact.min.css"; //core css
+import "primeicons/primeicons.css";
+import "/node_modules/primeflex/primeflex.css";
+import ScatterPlot from "./Scatterplots/ScatterPlot";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ScatterPlot />
+
     </div>
   );
-}
+};
 
 export default App;
